db6e50a942a3095377c01366ec0378e2e8e28885
Somashree Ghosh <somashreeghosh13@gmail.com>
2026-09-17 15:22:18 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        freq = {}
+        if len(s) != len(t):
+            return False
+        for i in range(len(s)):
+            freq[s[i]] = freq.get(s[i],0)+1
+            freq[t[i]] = freq.get(t[i],0)-1
+        for value in freq.values():
+            if value != 0:
+                return False
+        return True 
